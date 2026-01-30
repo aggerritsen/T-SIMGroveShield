33389cc18c44f4095de9e256d23221af8b73ae6a
Arjen Gerritsen <ag.gerritsen@emergoit.nl>
2026-01-30 11:46:43 +0100

    REV.B

--- UARTStub/run.ps1
+++ UARTStub/run.ps1
@@ -1,2 +1,2 @@
 pio run -t clean -e xiao-esp32s3
-pio run -e xiao-esp32s3 -t upload -p COM4; if ($LASTEXITCODE -eq 0) { pio device monitor }
+pio run -e xiao-esp32s3 -t upload; if ($LASTEXITCODE -eq 0) { pio device monitor }
